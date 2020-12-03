c1ba83bf81c444be1d91c4f5b4bac1baa2ea807d
axel <axel.declerck@student.vinci.be>
2020-12-03 16:32:38 +0100

    package.json

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios'
 import './App.css';
 // .catch(error => console.log(error))
-// Script2 package.json --> "deploy": "cp -a BlockCovidWeb/build/. public/",
+// Script2 package.json --> "deploy": "cp -a BlockCovidWeb/build/. public/",  "build": "react-scripts build",
 const App = () => {
   useEffect(() => {
     axios.get('/api/bears')
