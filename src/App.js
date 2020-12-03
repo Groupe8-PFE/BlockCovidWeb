34f92163641c9fbfa32c4f904543797c2050b342
axel <axel.declerck@student.vinci.be>
2020-12-03 15:32:17 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios'
 import './App.css';
 
-// script package.json --> "build": "react-scripts build",
+// Script2 package.json --> "build": "react-scripts build",
 const App = () => {
   useEffect(() => {
     axios.get('/api/bears')
