704c8f6588ea45e4e1f9bb111b7333ffce9904e9
Samuel <vancampenhoutsamuel@gmail.com>
2020-12-04 16:51:52 +0100

    add - formulaire d'inscription

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import AppContainer from './components/App/AppContainer';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<AppContainer />, document.getElementById('root'))
